e380966e301503585a1edf8920ca3af5171cc74e
Vladimir Michael Eatwell <dimir@fb.com>
2021-06-29 11:44:21 +0100

    [watchos] pointer alignment

--- src/unix/bsd/apple/mod.rs
+++ src/unix/bsd/apple/mod.rs
@@ -4140,7 +4140,7 @@ extern "C" {
 }
 
 cfg_if! {
-    if #[cfg(any(target_arch = "arm", target_arch = "x86", target_os = "watchos"))] {
+    if #[cfg(any(target_arch = "arm", target_arch = "x86", target_pointer_width = "32"))] {
         mod b32;
         pub use self::b32::*;
     } else if #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))] {
